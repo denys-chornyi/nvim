f482432f317dd726fd31522a4b5ef89a23817142
ge32yom <ge32yom@mytum.de>
2024-09-11 16:46:05 +0200

    new file explorer added and configured

--- lua/dchorn/plugins/colorscheme.lua
+++ lua/dchorn/plugins/colorscheme.lua
@@ -4,6 +4,25 @@ return {
     "folke/tokyonight.nvim",
     priority = 1000, -- to make sure, that plugin loads before any other
     config = function()
+
+        -- local bg = ""
+        -- local bg_dark = ""
+        -- local bg_highlight = ""
+        -- local bg_search = ""
+        -- local bg_visual = ""
+        -- local fg = ""
+        -- local fg_dark = ""
+        -- local fg_gutter = ""
+        -- local border = ""
+        
+        require("tokyonight").setup({
+            style = "night"
+            -- on_colors = function(colors)
+            --    colors.bg = bg
+            --    ...
+            -- end
+        })
+
         vim.cmd("colorscheme tokyonight")
     end
 
