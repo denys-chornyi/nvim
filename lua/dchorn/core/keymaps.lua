f074d477bf6eab1889545878520a2d2641d4e403
ge32yom <ge32yom@mytum.de>
2024-10-13 21:14:46 +0200

    final plugins added

--- lua/dchorn/core/keymaps.lua
+++ lua/dchorn/core/keymaps.lua
@@ -47,7 +47,7 @@ keymap.set("n", "<leader>t", "<C-w>j", { desc = "Focus lower window" })
 
 -- for tabs managing
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
-keymap.set("n", "<leader>tx", "<cmd>tablose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
--- lua/dchorn/plugins/todo-comments.lua
+++ lua/dchorn/plugins/todo-comments.lua
@@ -1,21 +1,21 @@
 return {
-    "folke/todo-comments.nvim",
-    event = { "bufreadpre", "bufnewfile" },
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-        local todo_comments = require("todo-comments")
-    
-        -- set keymaps
-        local keymap = vim.keymap -- for conciseness
+	"folke/todo-comments.nvim",
+	event = { "bufreadpre", "bufnewfile" },
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local todo_comments = require("todo-comments")
 
-        keymap.set("n", "]t", function()
-            todo_comments.jump_next()
-        end, { desc = "next todo comment" })
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
 
-        keymap.set("n", "[t", function()
-            todo_comments.jump_prev()
-        end, { desc = "previous todo comment" })
+		keymap.set("n", "]t", function()
+			todo_comments.jump_next()
+		end, { desc = "next todo comment" })
 
-        todo_comments.setup()
-    end,
+		keymap.set("n", "[t", function()
+			todo_comments.jump_prev()
+		end, { desc = "previous todo comment" })
+
+		todo_comments.setup()
+	end,
 }
